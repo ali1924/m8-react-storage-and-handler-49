5f4fcd436a937d90170df27b72008c185959a331
ali1924 <mohammadali229987@gmail.com>
2023-04-06 10:31:35 +0600

    49_5-1 Module Introduction ES6 Modules simple import, export

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cosmetics.css';
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>This is Cosmetics</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
