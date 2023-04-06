dcec19f8b0f81f09c8605a38190560409bc466f5
ali1924 <mohammadali229987@gmail.com>
2023-04-06 12:19:47 +0600

    onclick with parameter same file

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Cosmetic.css';
 const Cosmetic = (props) => {
+    const product = props.cosmetic;
     // console.log(props.cosmetic);
-    const { id, name, price } = props.cosmetic;
+    const { id, name, price } =product;
+  
+    
+    // handler cart
+    const addToCart =(product)=>{
+        console.log('added',product);
+    }
     return (
-        <div>
+        <div className='product'>
             <h2>Buy This:{name}</h2>
             <p>Only for :${price}</p>
+            {/* <button onClick={addToCart}>Add To Cart</button> */}
+            <button onClick={()=>addToCart(product)}>Add To Cart</button>
         </div>
     );
 };
