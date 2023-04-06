b677a03b5431f10be204526eef79f55c85198419
ali1924 <mohammadali229987@gmail.com>
2023-04-06 12:08:30 +0600

    49_5-3 Create and Load Fake data, json generator, online api

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cosmetics.css';
 import Cosmetic from '../Cosmetic/Cosmetic';
 const Cosmetics = () => {
-    //create local data
-    const cosmetics = [
-        { id:1,name:'alta',price:100},
-        { id:2,name:'alta',price:100},
-        { id:3,name:'alta',price:100},
-        { id:4,name:'alta',price:100},
-        { id:5,name:'alta',price:100},
-    ]
+    // create local data online json generator
+    const [cosmetics, setCosmetics] = useState([]);
+    useEffect(() => {
+        fetch('data.json').then(res => res.json()).then(data => setCosmetics(data));
+    },[])
+    //create local data offline
+    // const cosmetics = [
+    //     { id:1,name:'alta',price:100},
+    //     { id:2,name:'alta',price:100},
+    //     { id:3,name:'alta',price:100},
+    //     { id:4,name:'alta',price:100},
+    //     { id:5,name:'alta',price:100},
+    // ]
     return (
         <div>
             <h1>Welcome to my Cosmetic store</h1>
